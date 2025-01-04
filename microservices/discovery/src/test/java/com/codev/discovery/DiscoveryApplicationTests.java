870eabecf2bf9c68d7421609010e11017dad725a
codec-13/ <barhamadieng66@gmail.com>
2025-01-04 21:46:37 +0000

    feat(discovery): ajout du service discovery

---
+++ microservices/discovery/src/test/java/com/codev/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codev.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
