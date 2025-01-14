ec65db125329249bc9341aa5f9a01f72f0adf4d9
codec-13/ <barhamadieng66@gmail.com>
2025-01-14 10:18:59 +0000

    fix(services): faire correspondre le path des services dans les controller

--- microservices/order/src/main/java/com/codev/ecommerce/order/OrderController.java
+++ microservices/order/src/main/java/com/codev/ecommerce/order/OrderController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 public class OrderController {
     private final OrderService service;
 
